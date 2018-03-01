10efd05b577ed3c04271e55dca797f19cc800012
Mariusz <wadas.mariusz94@gmail.com>
2018-03-01 13:26:56 +0100

    *modified display tree File using jstree *added searching node in tree *added js file to display interactive tree

--- src/main/java/pl/zmt/menager/controllers/IndexController.java
+++ src/main/java/pl/zmt/menager/controllers/IndexController.java
@@ -1,14 +1,16 @@
 package pl.zmt.menager.controllers;
 
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import javax.servlet.http.HttpServletResponse;
+
 @RestController
-@RequestMapping("/index")
 public class IndexController {
 
-    @RequestMapping(method = RequestMethod.GET)
+    @RequestMapping(value = "/index", method = RequestMethod.GET)
     public String showMainPage(){
         return "index";
     }
---
+++ src/main/resources/static/js/showTree.js
@@ -0,0 +1,25 @@
+$(function () {
+
+    $('#jstree').jstree({
+        "core" : {
+            "themes" : { "stripes" : true }
+        },
+        "types" : {
+            "default" : {
+                "icon" : false
+            }
+        },
+        "plugins" : [
+            "search",
+            "types",
+            "wholerow"
+        ]
+    });
+    $('#jstree').on("changed.jstree", function (e, data) {
+        console.log(data.selected);
+    });
+
+    $('#search').keyup(function(){
+        $('#jstree').jstree('search', $(this).val());
+    });
+});
